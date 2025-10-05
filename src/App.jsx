f6fd1f2f4abab288d8def0a3a199778019a59658
Marianna-Dalterio <mariannadalterio00@gmail.com>
2025-10-05 17:59:33 +0200

    complete milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,43 @@
 // Impostare il lavoro su più componenti.
 
 import list from "./data/list";
-
+import { useState } from "react";
 
 
 function App() {
+  const [articles, setArticles] = useState(list); // con la lista 
+  const [newArticle, setNewArticle] = useState(""); //quello che l'utente digita
 
+  //gestire il cambiamento dell'input
+  const handleInput = (event) => {
+    setNewArticle(event.target.value);
+  };
+  //gestisce l'invio del form prevenendo il caricamento di default e aggiornando la lista con la creazione di un nuovo array
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setArticles([...articles, { id: Date.now(), title: newArticle }]);
+    // Resetta lo stato dell'input (svuota il campo dopo l'aggiunta)
+    setNewItem('');
+  }
 
   return (
     <>
       <h2>Lista di articoli</h2>
+
+      {/* aggiungo in pagina il form con input  */}
+
+      <form onSubmit={handleSubmit}>
+        <input type="text"
+          placeholder="Aggiungi un nuovo articolo"
+          value={newArticle}
+          onChange={handleInput} />
+
+        <button type="submit">Aggiungi</button>
+      </form>
+
       {/* creo lista articoli che mostri solo il titolo */}
       <ul>
-        {list.map((item, id) => <li key={id}>{item.title}</li>)}
+        {articles.map((item) => <li key={item.id}>{item.title}</li>)}
       </ul>
 
     </>
